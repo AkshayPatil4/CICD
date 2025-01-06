ce69bfbe31167521bb9b28e6e761fc3259921528
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2025-01-06 21:21:25 +0100

    angular test

--- angular-frontend/karma.conf.js
+++ angular-frontend/karma.conf.js
@@ -1,46 +1,45 @@
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma'),
-      ],
-      client: {
-        jasmine: {},
-        clearContext: false, // leave Jasmine Spec Runner output visible in browser
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false, // Turn off auto-watching for CI pipelines
+    browsers: ['ChromeHeadlessNoSandbox'], // Use custom launcher for headless Chrome
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-extensions',
+          '--disable-gpu',
+          '--disable-software-rasterizer',
+        ],
       },
-      jasmineHtmlReporter: {
-        suppressAll: true, // removes the duplicated traces
-      },
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage'),
-        subdir: '.',
-        reporters: [{ type: 'html' }, { type: 'text-summary' }],
-      },
-      reporters: ['progress', 'kjhtml'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['ChromeHeadless'], // Use ChromeHeadless for stability
-      customLaunchers: {
-        ChromeHeadlessNoSandbox: {
-          base: 'ChromeHeadless',
-          flags: [
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-extensions',
-            '--disable-gpu',
-            '--disable-software-rasterizer',
-          ],
-        },
-      },
-      singleRun: false,
-      restartOnFileChange: true,
-    });
-  };
-  
+    },
+    singleRun: true, // Exit after running tests
+    restartOnFileChange: false,
+  });
+};
